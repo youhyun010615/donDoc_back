afa4be2697bc3a03cb48bf9020379a8c3e2ce8b8
유현 <cadry238@gmail.com>
2026-04-24 17:07:04 +0900

    초기 작업 세팅

---
+++ src/main/java/com/dondoc/DondocApplication.java
@@ -0,0 +1,13 @@
+package com.dondoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DondocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DondocApplication.class, args);
+	}
+
+}
